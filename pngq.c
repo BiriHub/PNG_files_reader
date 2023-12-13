d1ea7812eee89b4a69baa221fa45be09b121fed2
Biri <leonardo09012003@gmail.com>
2023-12-13 16:30:58 +0100

    Added "pngq.c" file

---
+++ pngq.c
@@ -0,0 +1,7 @@
+/*
+Systems Programming assignment : Reading Data from Portable Network Graphics Files
+
+Write a C program named "pngq.c" that reads and extracts information from a Portable Network Graphic file (PNG).
+PNG format specifications are available on the W3C documentation : https://www.w3.org/TR/2023/CR-png-3-20230921/
+
+*/
